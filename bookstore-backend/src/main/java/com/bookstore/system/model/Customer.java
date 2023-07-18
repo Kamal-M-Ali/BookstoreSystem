f0504f6ec50e767482cf1fcd89d9537f4e571884
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-18 18:26:02 -0400

    fixed payment card bug

--- bookstore-backend/src/main/java/com/bookstore/system/model/Customer.java
+++ bookstore-backend/src/main/java/com/bookstore/system/model/Customer.java
@@ -1,11 +1,14 @@
 package com.bookstore.system.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
@@ -23,7 +26,8 @@ public class Customer extends User {
     @Valid
     private Address address;
 
-    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @NotNull(message = "Must add a payment card")
     @Valid
     private Set<PaymentCard> paymentCards;

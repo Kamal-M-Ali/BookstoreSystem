f89f76ea0c44e67f152e196d1b3a354432bbd06d
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-18 15:06:55 -0400

    add unauthorized admin routing

--- bookstore-frontend/src/App.js
+++ bookstore-frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import Home from './UserHome/Home';
 import ErrorPage from './ErrorPage';
@@ -20,15 +21,36 @@ import View from './Shop/View';
 import Cart from './Checkout/Cart';
 import Checkout from './Checkout/Checkout'
 import ResetPassword from './Register/ResetPassword';
+import Unauthorized from './Unauthorized';
 
 export default function App() {
   const key = 'jwtToken'
+
+  const [adminStatus, setAdminStatus] = useState(false);
+  const API = 'http://localhost:8080/api/admin-menu?email=';
+
+  useEffect(()=>{
+    const email = localStorage.getItem('email') || sessionStorage.getItem('email');
+
+    if (email) {
+      axios.get(API + email)
+        .then((res) => {
+          setAdminStatus(true);
+        })
+        .catch((err) => {
+          console.log(err.response);
+          setAdminStatus(false);
+        })
+    } else {
+      setAdminStatus(false);
+    }
+  }, [])
 
   useEffect(() => {
     const token = localStorage.getItem(key) || sessionStorage.getItem(key);
 
     if (token) {
-      axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+      axios.defaults.headers.common = { 'Authorization': `Bearer ${token}` }
     }
   })
 
@@ -45,12 +67,12 @@ export default function App() {
         <Route path='/ForgotPassword' element={<ForgotPassword />} />
         <Route path='/Account' element={<AccountPage />} />
         <Route path='/Account/OrderHistory' element={<OrderHistory />} />
-        <Route path='/Admin' element={<Admin />} />
-        <Route path='/Admin/ManageBooks/Add' element={<AddBook />} />
-        <Route path='/Admin/ManageBooks/Remove' element={<RemoveBook />} />
-        <Route path='Admin/ManageBooks/RemoveBook/:isbn' element={<Remove />} />
-        <Route path='Admin/ManageBooks/UpdateBook/:isbn' element={<Update />} />
-        <Route path='/Admin/ManageBooks/Update' element={<UpdateBook />} />
+        <Route path='/Admin' element={adminStatus ? <Admin /> : <Unauthorized />} />
+        <Route path='/Admin/ManageBooks/Add' element={adminStatus ? <AddBook /> : <Unauthorized />} />
+        <Route path='/Admin/ManageBooks/Remove' element={adminStatus ? <RemoveBook /> : <Unauthorized />} />
+        <Route path='Admin/ManageBooks/RemoveBook/:isbn' element={adminStatus ? <Remove /> : <Unauthorized />} />
+        <Route path='Admin/ManageBooks/UpdateBook/:isbn' element={adminStatus ? <Update /> : <Unauthorized />} />
+        <Route path='/Admin/ManageBooks/Update' element={adminStatus ? <UpdateBook /> : <Unauthorized />} />
         <Route path='/ResetPassword/:token' element={<ResetPassword />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>

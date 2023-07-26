a7e341388a3293a14c661e60bcd28cbc70404b19
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-26 17:21:50 -0400

    use book details

--- bookstore-frontend/src/Book.js
+++ bookstore-frontend/src/Book.js
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 
 export default function Book(props)
 {
+    console.log(props);
     return (
         <Card className='book'>
-            <img src={props.cover} alt={props.description} className='cover-img'/>
-            <h2 className='book-listing'>{props.title}<br />by: {props.author}<br />[{props.rating} / 5⭐]</h2>
-            <h2 className='book-price'>${props.price}</h2>
+            <img src={props.details.coverURL} alt={props.details.title} className='cover-img'/>
+            <h2 className='book-listing'>{props.details.title}<br />by: {props.details.author}<br />[{props.details.rating} / 5⭐]</h2>
+            <h2 className='book-price'>${props.details.price}</h2>
 
             <span className='buttonSpan'>
-                <Link to={`/View/${props.isbn}`} state={{book: props}}>
+                <Link to={`/View/${props.details.isbn}`} state={{book: props.details}}>
                     <button className='view-details-btn'>View Details</button>
                 </Link>
                 <div className='addToCart'>

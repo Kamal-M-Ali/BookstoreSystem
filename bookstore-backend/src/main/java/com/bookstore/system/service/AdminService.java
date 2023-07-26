8e0842f78a132c1c7896369dbdc68a30cf0a9f4d
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-26 14:34:22 -0400

    added admin/book service

---
+++ bookstore-backend/src/main/java/com/bookstore/system/service/AdminService.java
@@ -0,0 +1,72 @@
+package com.bookstore.system.service;
+
+import com.bookstore.system.model.Admin;
+import com.bookstore.system.model.Login;
+import com.bookstore.system.repository.AdminRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public ResponseEntity<String> doesAdminExist(String email) {
+        Admin admin = adminRepository.findByEmail(email);
+
+        if (admin != null)
+            return ResponseEntity.ok().body("Admin found");
+        return ResponseEntity.badRequest().body("Invalid Credentials");
+    }
+
+    @Bean
+    public ResponseEntity<String> newAdmin(Admin newAdmin) {
+        if (adminRepository.findByEmail(newAdmin.getEmail()) != null)
+            return ResponseEntity.badRequest().body("Account already exists");
+
+        if (newAdmin.getPassword().length() < 6)
+            return ResponseEntity.badRequest().body("Password too short");
+
+        // encrypt their password
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        newAdmin.setPassword(passwordEncoder.encode(newAdmin.getPassword()));
+
+        // save customer in database
+        adminRepository.save(newAdmin);
+        String jwtToken = jwtService.generateToken(newAdmin);
+
+        // account creation success
+        return ResponseEntity.ok().body("Account creation success");
+    }
+
+    @Bean
+    public ResponseEntity<String> adminLogin(Login login) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        login.getEmail(),
+                        login.getPassword()
+                )
+        );
+
+        Admin admin = adminRepository.findByEmail(login.getEmail());
+
+        if (admin != null) {
+            // if here then user is authenticated and exists
+            return ResponseEntity.ok().body(jwtService.generateToken(admin));
+        }
+        return ResponseEntity.badRequest().body("Invalid Credentials");
+    }
+}
---
+++ bookstore-backend/src/main/java/com/bookstore/system/service/BookService.java
@@ -0,0 +1,27 @@
+package com.bookstore.system.service;
+
+import com.bookstore.system.exception.BookNotFoundException;
+import com.bookstore.system.model.Book;
+import com.bookstore.system.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Bean
+    public Book getBook(Integer id) {
+        return bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @Bean
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}

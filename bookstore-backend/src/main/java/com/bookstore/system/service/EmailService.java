1f867dd82cc6e445eec8327ccdf1ef41e039d244
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-26 14:35:06 -0400

    updated sendEmail method

--- bookstore-backend/src/main/java/com/bookstore/system/service/EmailService.java
+++ bookstore-backend/src/main/java/com/bookstore/system/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.bookstore.system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -17,8 +18,16 @@ public class EmailService {
     }
 
     @Async
-    public void sendEmail(SimpleMailMessage email) {
+    private void sendEmail(SimpleMailMessage email) {
         javaMailSender.send(email);
     }
 
+    @Bean
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(text);
+        sendEmail(mailMessage);
+    }
 }

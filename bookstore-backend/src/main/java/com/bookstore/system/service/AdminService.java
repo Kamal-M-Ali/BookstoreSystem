1aca05a9cf6e6941189a632440e52418fe6dad69
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-26 15:42:58 -0400

    password service

--- bookstore-backend/src/main/java/com/bookstore/system/service/AdminService.java
+++ bookstore-backend/src/main/java/com/bookstore/system/service/AdminService.java
@@ -3,44 +3,32 @@ package com.bookstore.system.service;
 import com.bookstore.system.model.Admin;
 import com.bookstore.system.model.Login;
 import com.bookstore.system.repository.AdminRepository;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class AdminService {
     @Autowired
     private AdminRepository adminRepository;
     @Autowired
+    private PasswordService passwordService;
+    @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     private JwtService jwtService;
-
-    public ResponseEntity<String> doesAdminExist(String email) {
-        Admin admin = adminRepository.findByEmail(email);
-
-        if (admin != null)
-            return ResponseEntity.ok().body("Admin found");
-        return ResponseEntity.badRequest().body("Invalid Credentials");
-    }
 
     public ResponseEntity<String> newAdmin(Admin newAdmin) {
         if (adminRepository.findByEmail(newAdmin.getEmail()) != null)
             return ResponseEntity.badRequest().body("Account already exists");
 
-        if (newAdmin.getPassword().length() < 6)
-            return ResponseEntity.badRequest().body("Password too short");
-
-        // encrypt their password
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        newAdmin.setPassword(passwordEncoder.encode(newAdmin.getPassword()));
+        if (!passwordService.isValidPassword(newAdmin.getPassword()))
+            return ResponseEntity.badRequest().body("Password invalid");
+        newAdmin.setPassword(
+                passwordService.encryptPassword(newAdmin.getPassword())
+        );
 
         // save customer in database
         adminRepository.save(newAdmin);
@@ -64,6 +52,14 @@ public class AdminService {
             // if here then user is authenticated and exists
             return ResponseEntity.ok().body(jwtService.generateToken(admin));
         }
+        return ResponseEntity.badRequest().body("Invalid Credentials");
+    }
+
+    public ResponseEntity<String> doesAdminExist(String email) {
+        Admin admin = adminRepository.findByEmail(email);
+
+        if (admin != null)
+            return ResponseEntity.ok().body("Admin found");
         return ResponseEntity.badRequest().body("Invalid Credentials");
     }
 }

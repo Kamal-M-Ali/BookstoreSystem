3f4535394deb258316d45cbd2eeb1de66255dccf
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-27 19:37:35 -0400

    add missing methods

--- bookstore-backend/src/main/java/com/bookstore/system/service/EmailService.java
+++ bookstore-backend/src/main/java/com/bookstore/system/service/EmailService.java
@@ -2,10 +2,13 @@ package com.bookstore.system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
 
 @Service
 public class EmailService {
@@ -21,12 +24,23 @@ public class EmailService {
     private void sendEmail(SimpleMailMessage email) {
         javaMailSender.send(email);
     }
-    
+
+
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(to);
         mailMessage.setSubject(subject);
         mailMessage.setText(text);
         sendEmail(mailMessage);
+    }
+
+    public boolean validateEmail(String email) {
+        // regex source: https://www.baeldung.com/java-email-validation-regex
+        if (!Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+                .matcher(email)
+                .matches())
+            return true;
+
+        return false;
     }
 }

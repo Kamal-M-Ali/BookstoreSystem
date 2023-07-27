a4fd35725effe6c442823bfda303fb0dcff10bac
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-27 01:47:14 -0400

    only logged in users can manage cart

--- bookstore-frontend/src/Checkout/OrderItem.js
+++ bookstore-frontend/src/Checkout/OrderItem.js
@@ -1,19 +1,23 @@
 import './OrderItem.css';
 import Card from '../Card';
 
-export default function OrderItem(props)
-{
+export default function OrderItem(props) {
+    const key = 'jwtToken';
+    let loggedIn = sessionStorage.getItem(key) || localStorage.getItem(key) || false;
+
     return (
         <Card className='order-item'>
-            <img src={props.cover} alt={props.description} className='order-cover-img'/>
+            <img src={props.cover} alt={props.description} className='order-cover-img' />
             <h2 className='order-book-listing'>{props.title} by: {props.author} [{props.rating} / 5⭐]</h2>
             <h2 className='order-book-price'>${props.price}</h2>
             <h2 className='order-book-qty'>Quantity: 1</h2>
 
-            <div className='confirm-checkout-btns'>
-                <button className='cart-btn add'>Add</button>
-                <button className='cart-btn rm'>Remove</button>
-            </div>
+            {loggedIn &&
+                <div className='confirm-checkout-btns'>
+                    <button className='cart-btn add'>Add</button>
+                    <button className='cart-btn rm'>Remove</button>
+                </div>
+            }
         </Card>
     );
 }

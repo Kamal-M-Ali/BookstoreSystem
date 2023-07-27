2c3cb26aadc1c154a9d1db3620a37d77f414ce59
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-27 19:18:13 -0400

    finished manage cart functionality

--- bookstore-frontend/src/Checkout/OrderItem.js
+++ bookstore-frontend/src/Checkout/OrderItem.js
@@ -1,23 +1,55 @@
 import './OrderItem.css';
 import Card from '../Card';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function OrderItem(props) {
     const key = 'jwtToken';
     let loggedIn = sessionStorage.getItem(key) || localStorage.getItem(key) || false;
+    const [qty, setQty] = useState(props.qty);
+
+    function handleIncrement() {
+        const email = localStorage.getItem('email') || sessionStorage.getItem('email');
+
+        if (email) {
+            axios.put("http://localhost:8080/api/cart-book/:" + email, props.details)
+                .then((res) => {
+                    setQty(qty + 1);
+                    props.updateQty();;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    }
+
+    function handleDecrement() {
+        const email = localStorage.getItem('email') || sessionStorage.getItem('email');
+        if (email) {
+            axios.post("http://localhost:8080/api/del-cart-book/:" + email, props.details)
+                .then((res) => {
+                    setQty(qty - 1);
+                    props.updateQty();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    }
 
     return (
-        <Card className='order-item'>
+        (qty > 0 && <Card className='order-item'>
             <img src={props.details.coverURL} alt={props.details.title} className='order-cover-img' />
             <h2 className='order-book-listing'>{props.details.title} by: {props.details.author} [{props.details.rating} / 5⭐]</h2>
             <h2 className='order-book-price'>${props.details.price}</h2>
-            <h2 className='order-book-qty'>Quantity: 1</h2>
+            <h2 className='order-book-qty'>Quantity: {qty}</h2>
 
             {loggedIn &&
                 <div className='confirm-checkout-btns'>
-                    <button className='cart-btn add'>Add</button>
-                    <button className='cart-btn rm'>Remove</button>
+                    <button className='cart-btn add' onClick={handleIncrement}>Add</button>
+                    <button className='cart-btn rm' onClick={handleDecrement}>Remove</button>
                 </div>
             }
-        </Card>
+        </Card>)
     );
 }

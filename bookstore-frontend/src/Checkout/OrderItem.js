64cd8f4845e701497a2c74cb623256a30ea5fd28
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-27 20:55:08 -0400

    make order item buttons update cart

--- bookstore-frontend/src/Checkout/OrderItem.js
+++ bookstore-frontend/src/Checkout/OrderItem.js
@@ -1,12 +1,10 @@
 import './OrderItem.css';
 import Card from '../Card';
 import axios from 'axios';
-import { useState } from 'react';
 
 export default function OrderItem(props) {
     const key = 'jwtToken';
     let loggedIn = sessionStorage.getItem(key) || localStorage.getItem(key) || false;
-    const [qty, setQty] = useState(props.qty);
 
     function handleIncrement() {
         const email = localStorage.getItem('email') || sessionStorage.getItem('email');
@@ -14,7 +12,6 @@ export default function OrderItem(props) {
         if (email) {
             axios.put("http://localhost:8080/api/cart-book/:" + email, props.details)
                 .then((res) => {
-                    setQty(qty + 1);
                     props.updateQty();;
                 })
                 .catch((err) => {
@@ -28,7 +25,6 @@ export default function OrderItem(props) {
         if (email) {
             axios.post("http://localhost:8080/api/del-cart-book/:" + email, props.details)
                 .then((res) => {
-                    setQty(qty - 1);
                     props.updateQty();
                 })
                 .catch((err) => {
@@ -38,11 +34,11 @@ export default function OrderItem(props) {
     }
 
     return (
-        (qty > 0 && <Card className='order-item'>
+        <Card className='order-item'>
             <img src={props.details.coverURL} alt={props.details.title} className='order-cover-img' />
             <h2 className='order-book-listing'>{props.details.title} by: {props.details.author} [{props.details.rating} / 5⭐]</h2>
             <h2 className='order-book-price'>${props.details.price}</h2>
-            <h2 className='order-book-qty'>Quantity: {qty}</h2>
+            <h2 className='order-book-qty'>Quantity: {props.quantity}</h2>
 
             {loggedIn &&
                 <div className='confirm-checkout-btns'>
@@ -50,6 +46,6 @@ export default function OrderItem(props) {
                     <button className='cart-btn rm' onClick={handleDecrement}>Remove</button>
                 </div>
             }
-        </Card>)
+        </Card>
     );
 }

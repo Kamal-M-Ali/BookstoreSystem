65edba306ce65e685132a65c819cb0e2d0372e81
Kamal <99984032+Kamal-M-Ali@users.noreply.github.com>
2023-07-30 13:46:33 -0400

    added promotion controller

---
+++ bookstore-backend/src/main/java/com/bookstore/system/controller/PromotionController.java
@@ -0,0 +1,19 @@
+package com.bookstore.system.controller;
+
+import com.bookstore.system.service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class PromotionController {
+    @Autowired
+    private PromotionService PromotionService;
+
+    @GetMapping("/api/get-promo")
+    public ResponseEntity<?> getPromo(@RequestParam("code") String code) {
+        return PromotionService.getPromo(code);
+    }
+
+}
